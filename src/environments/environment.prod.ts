8c4f143cf87cfa3135e35edbedf39f2757a5a938
sgonzmart2 <108746541+sgonzmart2@users.noreply.github.com>
2022-07-08 11:54:32 +0200

    Add files via upload

---
+++ src/environments/environment.prod.ts
@@ -0,0 +1,14 @@
+export const environment = {
+  production: true,
+  name: "",
+  PROD_URL_GATEWAY: 'http://pu.ijs.si',
+  firebase: {
+    apiKey: "",
+    authDomain: "",
+    databaseURL: "",
+    projectId: "",
+    storageBucket: "",
+    messagingSenderId: "",
+    appId: ""
+  }
+};
